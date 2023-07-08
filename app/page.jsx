8e5f2c6bc7c0b627c83d06a51632ad83724022de
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-08 13:58:23 +0300

    add app

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,16 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover & share
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center">AI-Powered prompts</span>
+      </h1>
+      <p className="dec text-center">
+        Promptopia is an open source AI Powered prompt generator for modern world to 
+        discover, create and share AI generated prompts.
+      </p>
+    </section>
   )
 }
 
