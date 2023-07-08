b61d2902ee7e9ced0892cc2badc9da2aa738d417
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-08 13:04:52 +0300

    Add layout and home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import '@styles/global.css';
+
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover and share AI-generated prompts",
+}
+
 const layout = () => {
   return (
     <div>layout</div>
