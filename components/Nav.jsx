1eaeddedbde138da2f3ac38a0a23eecf3024f814
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-08 15:34:13 +0300

    Add components

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Nav = () => {
   return (
-    <nav className='flex-between w-full mb-16'>Nav</nav>
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href="/" className='flex gap-2 flex-center'>
+        <Image src="/assets/images/logo.svg"
+         alt="Promptopia"
+         width={40}
+         height={40}
+         className='object-contain'
+          />
+          <p className='logo_text'>Promptia</p>
+      </Link>
+      <div className="sm:flex hidden"></div>
+    </nav>
   )
 }
 
