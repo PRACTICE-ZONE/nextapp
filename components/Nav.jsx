c3cc982053bcd0cfa1fe5b797355dd899ddbe1eb
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-18 23:23:52 +0300

    Add providers

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,19 @@
 'use client';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { signOut } from 'next-auth/react';
 
 const Nav = () => {
   const isUserLoggedIn = true;
+  const [provider, setProvider] = useState(null);
+
+  useEffect(() => {
+  
+  
+  }, [])
+  
+
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href="/" className='flex gap-2 flex-center'>
