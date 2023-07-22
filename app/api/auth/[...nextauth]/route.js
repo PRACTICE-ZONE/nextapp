9ad23cef1d295f8ff09026ac67ea944faea07481
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-22 23:06:16 +0300

    Add auth route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,14 @@
-import {  NextAuth } from "next-auth/next";
+
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
 
 const handler = NextAuth({
     // Configure one or more authentication providers
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }),
     ],
 
