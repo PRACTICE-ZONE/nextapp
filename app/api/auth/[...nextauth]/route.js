b051fa53396ab91e642289c8b5a1b0f508b7881c
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-22 22:46:42 +0300

    Add auth route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,24 +3,20 @@ import {  NextAuth } from "next-auth/next";
 const handler = NextAuth({
     // Configure one or more authentication providers
     providers: [
-        Providers.Google({
+        GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET
         }),
-        // ...add more providers here
     ],
-    // A database is optional, but required to persist accounts in a database
-    database: process.env.DATABASE_URL,
 
     // https://next-auth.js.org/configuration/callbacks
     callbacks: {
-        async signIn(user, account, profile) {
+        async session({session}) {
+            return true
+        },
+        async signIn( { profile }) {
             return true
         }
     }
 
-    // https://next-auth.js.org/configuration/options
-    // options: { ... }
-
-    
 })
