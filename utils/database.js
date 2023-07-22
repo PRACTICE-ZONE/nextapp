e65eb3708e7f5a9406a20e237ceaa0f3d73a0e1e
IBUNHABIBU <ibunhabib@gmail.com>
2023-07-22 23:21:31 +0300

    Add database

--- utils/database.js
+++ utils/database.js
@@ -3,14 +3,20 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 export const connectToDatabase = async () => {
+
+    mongoose.set('strictQuery', true)
     if (isConnected) {
         return;
     }
 
-    const db = await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-
-    isConnected = db.connections[0].readyState;
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        isConnected = true;
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
