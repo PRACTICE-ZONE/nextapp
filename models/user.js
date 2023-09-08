c930bcbc7e5a02060450a1792f60f7b4bcc4a24b
IBUNHABIBU <ibunhabib@gmail.com>
2023-09-08 23:00:02 +0300

    Signin user

--- models/user.js
+++ models/user.js
@@ -3,13 +3,13 @@ import { Schema, model, models } from "mongoose";
 const userSchema = new Schema({
     username: {
         type: String,
-        required: true,
+        required: [true, 'Username is required.'],
         unique: [true, 'Username already exists. Please try another one.'],
     },
 
     email: {
         type: String,
-        required: true,
+        required: [true, 'Email is required.'],
         unique: [true, 'Email already exists. Please try another one.'],
     },
 
