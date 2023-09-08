5f39f9b4bd72a604b5c9e60b2272a469626dbe22
IBUNHABIBU <ibunhabib@gmail.com>
2023-09-08 22:55:40 +0300

    Signin user

--- models/user.js
+++ models/user.js
@@ -1,1 +1,20 @@
 import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+
+    image: {
